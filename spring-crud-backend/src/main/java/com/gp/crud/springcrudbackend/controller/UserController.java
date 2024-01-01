f32644b1146fd3371bed046d2357d27b4c0de72b
Guna <Guna@192.168.1.53>
2024-01-01 16:53:16 +0400

    get user service added

--- spring-crud-backend/src/main/java/com/gp/crud/springcrudbackend/controller/UserController.java
+++ spring-crud-backend/src/main/java/com/gp/crud/springcrudbackend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.gp.crud.springcrudbackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +20,8 @@ public class UserController {
 		return userRepo.save(newUser);
 	}
 	
+	@GetMapping("/getUser")
+	User getUser(String userId) {
+		return userRepo.getById(null);
+	}
 }
